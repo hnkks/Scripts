775134de8ceccec772767e2830d1e09719f1d5e2
hnkks <100104194+hnkks@users.noreply.github.com>
2023-08-18 23:05:29 +0800

    Update 123.js

--- 123.js
+++ 123.js
@@ -20,11 +20,7 @@ hostname = *.hujuvod.*
 var body = $response.body;
 var url = $request.url;
 
-if (url.indexOf('/ad/getAll') != -1) {
-	var obj = JSON.parse(body);
-	obj.data.adList = [];
-	body = JSON.stringify(obj);
-} else if (url.indexOf('/user/personal/information') != -1) {
+if (url.indexOf('/user/personal/information') != -1) {
 	var obj = JSON.parse(body);
 	obj.data = [{
 		"action": "play",
