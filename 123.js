bb0f11d4b6021acbe4392cc6cf618b506b137ce1
hnkks <100104194+hnkks@users.noreply.github.com>
2023-08-18 22:08:15 +0800

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,87 @@
+/***********************************
+
+> 应用名称：
+> 脚本作者：hnkks
+> 解锁说明：
+> 更新时间：2023-8-
+> 特别说明：脚本仅供学习交流使用，禁止转载售卖
+          
+[rewrite_local]
+
+^http[s]?:\/\/api\.rr\.tv(\/user\/privilege\/list|\/ad\/getAll|\/rrtv-video\/v4plus\/season\/detail) url script-response-body https://raw.githubusercontent.com/hnkks/Scripts/main/123.js
+
+
+[mitm] 
+
+hostname = api.rr.tv
+
+***********************************/
+
+var body = $response.body;
+var url = $request.url;
+
+if (url.indexOf('/ad/getAll') != -1) {
+	var obj = JSON.parse(body);
+	obj.data.adList = [];
+	body = JSON.stringify(obj);
+} else if (url.indexOf('/user/privilege/list') != -1) {
+	var obj = JSON.parse(body);
+	obj.data = [{
+		"action": "play",
+		"effectObject": "video",
+		"id": 1,
+		"function": "originalPainting",
+		"func": "originalPainting",
+		"endTime": 1667341767582,
+		"description": "解锁原画",
+		"icon": "jiesuoyuanhua"
+	}, {
+		"action": "play",
+		"effectObject": "video",
+		"id": 4,
+		"function": "noLimit",
+		"func": "noLimit",
+		"endTime": 1567341767582,
+		"description": "看剧无限制",
+		"icon": "kanjuwuxianzhi"
+	}, {
+		"action": "play",
+		"effectObject": "growth",
+		"id": 37,
+		"function": "0.4",
+		"func": "0.4",
+		"endTime": 1667341767582,
+		"description": "看剧经验+40%",
+		"icon": "jingyanzhijiacheng"
+	}, {
+		"action": "send",
+		"effectObject": "danmu",
+		"id": 43,
+		"function": "superBarrageBlue",
+		"func": "superBarrageBlue",
+		"endTime": 1667341767582,
+		"description": "超级弹幕",
+		"icon": "chaojidanmu"
+	}, {
+		"action": "play",
+		"effectObject": "video",
+		"id": 23,
+		"function": "noAd",
+		"func": "noAd",
+		"endTime": 1667341767582,
+		"description": "看剧无广告",
+		"icon": "kanjuwuguanggao"
+	}];
+
+}else if(url.indexOf('/rrtv-video/v4plus/season/detail') != -1){
+	var obj = JSON.parse(body);
+
+	obj.data['season']['feeMode'] = 'restriction';
+}
+
+
+body = JSON.stringify(obj);
+
+$done({
+	body
+});
