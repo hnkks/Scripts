ea7bfeb833bd9163de3141f3cb8d7bf888699791
hnkks <100104194+hnkks@users.noreply.github.com>
2023-08-31 14:54:22 +0800

    Create ng.js

---
+++ ng.js
@@ -0,0 +1,22 @@
+/***********************************
+
+> 应用名称：ng
+> 脚本作者：hnkks
+> 解锁说明：不可描述，懂的都懂
+> 更新时间：2023-8-31
+> 特别说明：脚本仅供学习交流使用，禁止转载售卖
+          
+[rewrite_local]
+
+^http[s]?:\/\/open-secret-api-v2\.vcinema\.cn\/v5\.0\/user\/v2\/18848578 url script-response-body https://raw.githubusercontent.com/hnkks/Scripts/main/ng.js
+
+[mitm] 
+
+hostname = open-secret-api-v2.vcinema.cn
+
+***********************************/
+
+let obj = JSON.parse($response.body);
+obj.data.is_get = true;
+obj.data.get = true;
+$done({body: JSON.stringify(obj)});
